d6d3ebae0f6fe4e02e9b8208fdd4b80216e06cb2
Muhammad Ibrahim <muhammadibrahim5644@gmail.com>
2022-11-11 19:26:54 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { XAxis, YAxis, LineChart, Line,CartesianGrid } from "recharts";
+
+const pdata = [
+  {
+    name: "Python",
+    student: 13,
+    fees: 10,
+  },
+  {
+    name: "JavaScript",
+    student: 19,
+    fees: 9,
+  },
+  {
+    name: "PHP",
+    student: 25,
+    fees: 12,
+  },
+  {
+    name: "Java",
+    student: 40,
+    fees: 20,
+  },
+  {
+    name: "C#",
+    student: 10,
+    fees: 7,
+  },
+  {
+    name: "C++",
+    student: 50,
+    fees: 30,
+  },
+  {
+    name: "Assembly",
+    student: 4,
+    fees: 5,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="chart-heading">Line Chart</h1>
+     
+      <LineChart width={500} height={300} data={pdata}>
+        <XAxis dataKey="name" />
+        <YAxis/>
+        <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+        <Line type="monotone" dataKey="fees" stroke="#8884d8" />
+        <Line type="monotone" dataKey="fees" stroke="#82ca9d" />
+      </LineChart>
+    </>
   );
 }
 
